a338a9d02e43eca119ee5b2fafeff68050ada8c2
Teja <teja.rachakonda123@gmail.com>
2025-06-14 09:26:41 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,56 @@
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+import crud, models, schemas
+from database import SessionLocal, engine
+from fastapi.middleware.cors import CORSMiddleware
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    #allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+def get_db():
+    db = SessionLocal()
+    try: 
+        yield db
+    finally:
+        db.close()
+        
+@app.get("/")
+def read_root():
+    return {"message": "Todo app using PostgreSQl"}
+
+@app.get("/todos/", response_model=list[schemas.TodoRead])
+def read_todos(db: Session = Depends(get_db)):
+    return crud.get_todos(db)
+
+@app.post("/todos/", response_model=schemas.TodoRead)
+def create(todo: schemas.TodoCreate, db: Session = Depends(get_db)):
+    return crud.create_todo(db, todo)
+
+@app.get("/todos/{todo_id}", response_model=schemas.TodoRead)
+def read_todo(todo_id: int, db: Session = Depends(get_db)):
+    db_todo = crud.get_todos(db, todo_id)
+    if db_todo is None:
+        raise HTTPException(status_code=404, detail="Todo not found")
+    return db_todo
+
+@app.put("/todos/{todo_id}", response_model=schemas.TodoRead)
+def update(todo_id: int, todo: schemas.TodoCreate, db: Session = Depends(get_db)):
+    db_todo = crud.update_todo(db, todo_id, todo)
+    if db_todo is None:
+        raise HTTPException(status_code=404, detail="Todo not found")
+    return db_todo
+
+@app.delete("/todos/{todo_id}", response_model=schemas.TodoRead)
+def delete(todo_id: int, db: Session = Depends(get_db)):
+    db_todo = crud.delete_todo(db, todo_id)
+    if db_todo is None:
+        raise HTTPException(status_code=404, detail="Todo not found")
+    return db_todo
